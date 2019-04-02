f3f2587b6d45f6c7d118477e349492df004f7b33
yantze <yantze@126.com>
2019-04-02 23:36:01 +0800

    Support open file

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, dialog } = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -10,6 +10,7 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
+        show: false,
         webPreferences: {
             nodeIntegration: true,
         },
@@ -33,7 +34,14 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+    createWindow()
+
+    mainWindow.on('ready-to-show', () => {
+        mainWindow.show()
+        // getFilesFromUser()
+    })
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
@@ -50,3 +58,24 @@ app.on('activate', function() {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+function getFilesFromUser() {
+    const filePaths = dialog.showOpenDialog({
+        properties: ['openFile'],
+        filters: [
+            { name: 'Markdown files', extensions: ['md', 'markdown'] },
+            { name: 'Text Files', extensions: ['text'] },
+        ],
+    })
+
+    if (!filePaths) return
+
+    console.log('files:', filePaths)
+
+    mainWindow.webContents.send('file-opened', {
+        // ipcMain.emit('file-opened', {
+        filePaths,
+    })
+}
+
+app.getFilesFromUser = getFilesFromUser
