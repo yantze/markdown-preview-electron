77afd010e90416c0b92b1ff430a9c4f225a07424
yantze <yantze@126.com>
2019-04-11 09:49:29 +0800

    Make renderer as a Global object R

--- renderer.js
+++ renderer.js
@@ -2,6 +2,9 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
+const logger = {
+    debug: (...args) => console.log(...args),
+}
 
 class FileManager {
     constructor() {
@@ -9,38 +12,58 @@ class FileManager {
 
         const Dropbox = require('dropbox').Dropbox
         this.dbx = new Dropbox({ accessToken: process.env.DROPBOX_TOKEN })
+        // window.dbx = this.dbx
 
-        this.dbx.filesListFolder({path: ''}).then(listFolder => {
-            const isExistSource = listFolder.entries.some(foldInfo => foldInfo.path_lower === this.sourcePath)
+        this.dbx.filesListFolder({ path: '' }).then(listFolder => {
+            const isExistSource = listFolder.entries.some(
+                foldInfo => foldInfo.path_lower === this.sourcePath,
+            )
             if (!isExistSource) {
-                this.dbx.filesCreateFolderV2({path: this.sourcePath}).catch(err => {
-                    console.error(err)
-                })
+                this.dbx
+                    .filesCreateFolderV2({ path: this.sourcePath })
+                    .catch(err => {
+                        console.error(err)
+                    })
             }
         })
     }
 
     async getFileList() {
-        const listFiles = await this.dbx.filesListFolder({path: this.sourcePath})
+        const listFiles = await this.dbx.filesListFolder({
+            path: this.sourcePath,
+        })
         let fileList = listFiles.entries.map(fileInfo => fileInfo)
 
         return fileList
     }
 
     async writeFile(filePath, content) {
-        await this.dbx.filesUpload({path: filePath, contents: content})
+        const timestamp = Date.now()
+        try {
+            await this.dbx.filesGetMetadata({ path: filePath })
+            await this.dbx.filesDeleteV2({ path: filePath })
+        } catch (e) {
+            e
+        }
+
+        await this.dbx.filesUpload({ path: filePath, contents: content })
+        logger.debug('Write file cost:', Date.now() - timestamp, 'ms')
     }
 }
 
-async function demo() {
+// async function demo() {
+//     const fileManager = new FileManager()
+//     fileManager.getFileList().then(console.log)
 
-    const fileManager = new FileManager()
-    fileManager.getFileList().then(console.log)
+//     const content = 'bbbxx dddejjejxxxbbbb'
+//     const time = Date.now()
+//     await fileManager.writeFile('/source/test.md', content)
+//     console.log('Write file cost:', Date.now() - time, 'ms')
 
-    const content = 'bbbxx dddejjej'
-    fileManager.writeFile('/source/test.md', content)
+//     // const fileListEntries = await fileManager.getFileList()
+//     // console.log('fileList:', fileListEntries.map(e => e.path_display))
+// }
 
+module.exports = {
+    fileManager: new FileManager(),
 }
-
-demo()
-
